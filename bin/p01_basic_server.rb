2919de986e491b7bce5776cec93dcf905c38c6f1
Faye Keegan <faye.keegan@gmail.com>
2015-07-21 14:12:12 -0700

    built basic controller, phase 2 complete

--- bin/p01_basic_server.rb
+++ bin/p01_basic_server.rb
@@ -4,7 +4,7 @@ server = WEBrick::HTTPServer.new(:Port => 3000)
 
 server.mount_proc("/") do |request, response|
   response.content_type = "text/text"
-  response.body = request.path
+  response.body = request.path	
 end
 
 trap('INT') do
